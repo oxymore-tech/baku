6ba7fb84ba7b02ea0435528fa958afcbc5c84624
Antoine Le Squéren <alesqueren@liveaction.com>
2019-11-03 16:55:49 +0100

    front: fix compilation

---
+++ front/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  rules: {
+    "no-console": "off",
+    "no-debugger": "off"
+  },
+  parserOptions: {
+    parser: "@typescript-eslint/parser"
+  }
+};
