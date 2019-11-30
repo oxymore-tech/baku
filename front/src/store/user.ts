e469f30334f5cbaf50bc8e42beac0c96609caace
Jean-Baptiste Gamond <jeanbaptiste.gamond@livingobjects.com>
2019-11-30 12:14:31 +0100

    refactor: change username list

--- front/src/store/user.ts
+++ front/src/store/user.ts
@@ -2,34 +2,13 @@
 interface UserState {
   username: string;
 }
+const gfynonce = require('gfynonce');
 
-export const UsernameList: string[] = [
-  "American Virginia opossum",
-  "American alligator",
-  "American badger",
-  "American beaver",
-  "American bighorn sheep",
-  "American bison",
-  "American black bear",
-  "American buffalo",
-  "American crow",
-  "American marten",
-  "American racer",
-  "American woodcock",
-  "Anaconda",
-  "Andean goose",
-  "Ant",
-  "Anteater",
-  "Antechinus",
-  "Antelope",
-  "Arboral spiny rat",
-  "Arctic fox"
-];
 
 export const UserStore = {
   namespaced: true,
   state: {
-    username: UsernameList[Math.floor(Math.random() * Math.floor(UsernameList.length))] + ' (anonymous)'
+    username: gfynonce({ adjectives: 1, separator: ' ' }) + ' (anonymous)'
   },
   mutations: {
     changeUsername(state: UserState, name: string) {
