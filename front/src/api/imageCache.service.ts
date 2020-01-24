5d1d7dfb6ec9b2d893082507fe85bc678e07bedc
Antoine Le Squéren <alesqueren@liveaction.com>
2020-01-24 09:36:46 +0100

    tests multi download

--- front/src/api/imageCache.service.ts
+++ front/src/api/imageCache.service.ts
@@ -1,8 +1,66 @@
 import { Spinner } from '@/api/spinner.class';
 import { ImageRef, Quality } from './uploadedImage.class';
-import Axios from 'axios';
+// import Axios from 'axios';
 
 type ImgDict = { [id: string]: string };
+
+let tasks: any = []
+const executorNb = 5;
+for (let i = 0; i<executorNb; i++) {
+  pickNextTask(new Promise((r) => { r(); }), i);
+}
+
+// function wait1sec() {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve('resolved');
+//     }, 1000);
+//   });
+// }
+//
+// async function pickNextTask(executor: any, executorNb: number) {
+//   let task = tasks.shift();
+//   if (task) {
+//     console.log("[" + executorNb + "] processNewTask [" + tasks.length + "]");
+//     await task();
+//   } else {
+//     // console.log("[" + executorNb + "] wait 1 sec")
+//     await wait1sec();
+//   }
+//   executor.then(() => { pickNextTask(executor, executorNb) });
+// }
+
+
+function processNewTask(executor: any, executorNb: number) {
+    let task = tasks.shift();
+    if (task) {
+        console.log("processNewTask [" + tasks.length + "]");
+        executor
+            .then(task)
+            .then(() => { pickNextTask(executor, executorNb) });
+        return true
+    }
+    return false
+}
+
+function pickNextTask(executor: any, executorNb: number) {
+    return new Promise((resolve, reject) => {
+        if (processNewTask(executor, executorNb)) {
+            resolve()
+        } else {
+            // console.log("start timer");
+            let timer = setInterval(() => {
+                if (processNewTask(executor, executorNb)) {
+                    if (timer) {
+                        clearInterval(timer);
+                    }
+                    resolve();
+                }
+            }, 50)
+        }
+    })
+}
+
 
 class ImageCacheServiceImpl {
   private cachedImages: { [quality: string]: ImgDict } = {
@@ -19,15 +77,18 @@ class ImageCacheServiceImpl {
 
   public async startPreloading(imageRefs: ImageRef[], activeIndex: number) {
     const imageRefsSliced = imageRefs.slice(activeIndex).concat(imageRefs.slice(0, activeIndex));
-    await asyncForEach(imageRefsSliced, (async (image: ImageRef) => {
-      await this.preloadImage(image, Quality.Thumbnail);
-    }));
-    await asyncForEach(imageRefsSliced, (async (image: ImageRef) => {
-      await this.preloadImage(image, Quality.Lightweight);
-    }));
-    await asyncForEach(imageRefsSliced, (async (image: ImageRef) => {
-      await this.preloadImage(image, Quality.Original);
-    }));
+
+    // await asyncForEach(imageRefsSliced, (async (image: ImageRef) => {
+    // imageRefsSliced.forEach((image: ImageRef) => {
+    //   tasks.push(async () => { await this.preloadImage(image, Quality.Thumbnail) } )
+    // })
+    // imageRefsSliced.forEach((image: ImageRef) => {
+    //   tasks.push(async () => { await this.preloadImage(image, Quality.Lightweight) } )
+    // })
+    imageRefsSliced.forEach((image: ImageRef) => {
+      console.log("add!")
+      tasks.push(async () => { await this.preloadImage(image, Quality.Thumbnail) } )
+    })
   }
 
   public putImageInCache(imageRef: ImageRef) {
@@ -43,12 +104,13 @@ class ImageCacheServiceImpl {
     };
   }
 
-  private putImageB64InCacheInternal(imageRef: ImageRef, quality: Quality, b64: string) {
-    this.cachedImages[quality] = {
-      ...this.cachedImages[quality],
-      [imageRef.id]: b64,
-    };
-  }
+
+  // private putImageB64InCacheInternal(imageRef: ImageRef, quality: Quality, b64: string) {
+  //   this.cachedImages[quality] = {
+  //     ...this.cachedImages[quality],
+  //     [imageRef.id]: b64,
+  //   };
+  // }
 
   public putImageBlobInCache(imageId: string, image: string) {
     this.cachedImages[Quality.Original] = {
@@ -75,35 +137,35 @@ class ImageCacheServiceImpl {
   }
 
   private async preloadImage(image: ImageRef, quality: Quality) {
-    if (this.isQueryOngoing(image.id, quality)) {
-      return;
-    }
-    this.ongoingQueries[quality] = {
-      ...this.ongoingQueries[quality],
-      [image.id]: 'true'
-    };
-    const res = await Axios.get(image.getUrl(quality), { responseType: 'arraybuffer' }).catch(async (error) => {
-      await delay(2000);
-      return await Axios.get(image.getUrl(quality), { responseType: 'arraybuffer' });
-    });
-    let imgB64 = 'data:image/jpeg;base64,' + btoa(new Uint8Array(res.data).reduce((data, byte) => data + String.fromCharCode(byte), ''));
-    this.putImageB64InCacheInternal(image, quality, imgB64);
-    delete this.ongoingQueries[quality][image.id];
-    // return new Promise<void>((resolve) => {
-    //   if(this.isQueryOngoing(image.id, quality)) {
-    //     resolve();
-    //   }
-    //   this.ongoingQueries[quality] = {...this.ongoingQueries[quality],
-    //     [image.id]: 'true'
-    //   }
-    //   const tempImage = new Image();
-    //   tempImage.src = image.getUrl(quality);
-    //   tempImage.onload = () => {
-    //     this.putImageInCacheInternal(image, quality);
-    //     delete this.ongoingQueries[quality][image.id];
-    //     resolve();
-    //   };
+    // if (this.isQueryOngoing(image.id, quality)) {
+    //   return;
+    // }
+    // this.ongoingQueries[quality] = {
+    //   ...this.ongoingQueries[quality],
+    //   [image.id]: 'true'
+    // }
+    // const res = await Axios.get(image.getUrl(quality), { responseType: 'arraybuffer' }).catch(async (error) => {
+    //   await delay(2000);
+    //   return await Axios.get(image.getUrl(quality), { responseType: 'arraybuffer' });
     // });
+    // let imgB64 = 'data:image/jpeg;base64,' + btoa(new Uint8Array(res.data).reduce((data, byte) => data + String.fromCharCode(byte), ''));
+    // this.putImageB64InCacheInternal(image, quality, imgB64);
+    // delete this.ongoingQueries[quality][image.id];
+    return new Promise<void>((resolve) => {
+      if(this.isQueryOngoing(image.id, quality)) {
+        resolve();
+      }
+      this.ongoingQueries[quality] = {...this.ongoingQueries[quality],
+        [image.id]: 'true'
+      }
+      const tempImage = new Image();
+      tempImage.src = image.getUrl(quality);
+      tempImage.onload = () => {
+        this.putImageInCacheInternal(image, quality);
+        delete this.ongoingQueries[quality][image.id];
+        resolve();
+      };
+    });
   }
 
   public getImage(imageId: string): string {
@@ -137,10 +199,10 @@ class ImageCacheServiceImpl {
 
 export const ImageCacheService = new ImageCacheServiceImpl();
 
-async function asyncForEach(array: any[], callback: Function) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
+// async function asyncForEach(array: any[], callback: Function) {
+//   for (let index = 0; index < array.length; index++) {
+//     await callback(array[index], index, array);
+//   }
+// }
 
-const delay = (ms : number) => new Promise(res => setTimeout(res, ms));
+// const delay = (ms : number) => new Promise(res => setTimeout(res, ms));
