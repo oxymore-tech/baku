f4d0fcfdbb53bc4ea8d86908d81fcf3976c5741a
Augustin Gjini <augustin.gjini@pm.me>
2020-01-26 18:06:26 +0100

    deploy

--- deploy/generate-certificate.sh
+++ deploy/generate-certificate.sh
@@ -5,15 +5,15 @@ BASEDIR=$(dirname "$0")
 DOMAIN=app.bakuanimation.com
 EMAIL=augustin.gjini@pm.me
 
-if [[ ! -f ${BASEDIR}/certbot ]]; then
-    mkdir -p ${BASEDIR}/certbot
-fi
+mkdir -p ${BASEDIR}/certbot/conf
+mkdir -p ${BASEDIR}/certbot/www
 
 certbot certonly \
+        --config-dir "${BASEDIR}/certbot/conf" \
 		--agree-tos \
 		--domains "app.bakuanimation.com" \
 		--email "augustin.gjini@pm.me" \
 		--expand \
 		--noninteractive \
 		--webroot \
-		--webroot-path ${BASEDIR}/certbot || true
+		--webroot-path ${BASEDIR}/certbot/www || true
