7e3c2c8188b60b93afd0b7d9d878c3416c585b1f
Augustin Gjini <augustin.gjini@pm.me>
2020-01-26 16:53:09 +0100

    add deploy

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 
-docker-compose -p baku --project-directory ${BASEDIR}/.. -f ${BASEDIR}/docker-compose.yml up -d $*
+docker-compose -p baku --project-directory ${BASEDIR}/.. -f ${BASEDIR}/docker-compose.yml up -d --build $*
