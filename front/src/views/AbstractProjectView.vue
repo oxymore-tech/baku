db264860105ed00c2a6504c4214f1255cee493f7
Antoine Le Squéren <alesqueren@onestock-retail.com>
2020-04-18 18:36:53 +0200

    add CollaborationView to AbstractProjectView

--- front/src/views/AbstractProjectView.vue
+++ front/src/views/AbstractProjectView.vue
@@ -5,14 +5,21 @@ import { namespace } from 'vuex-class';
 const ProjectNS = namespace('project');
 
 @Component
+// TODO: find a better class name
 export default class Project extends Vue {
+  @ProjectNS.State
+  public id!: string;
+
   @ProjectNS.Action('loadProject')
   protected loadProjectAction!: (projectId: string) => Promise<void>;
 
   public async created() {
     const { projectId } = this.$route.params;
     if (projectId) {
-      await this.loadProjectAction(projectId);
+      // TODO: do not load project every time
+      // if (this.id == "") {
+        await this.loadProjectAction(projectId);
+      // }
     } else {
       await this.$router.push({ name: 'home' });
     }
