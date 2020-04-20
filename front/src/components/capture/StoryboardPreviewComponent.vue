f1d65c4280c98f75bebe266b11567980df8a4c62
Augustin Gjini <augustin.gjini@pm.me>
2020-04-20 18:13:32 +0200

    Display Plan number in Capture

--- front/src/components/capture/StoryboardPreviewComponent.vue
+++ front/src/components/capture/StoryboardPreviewComponent.vue
@@ -1,106 +1,115 @@
 <template>
   <div class="box-container storyboard-preview-container">
     <div class="storyboard-preview-header" @click="onDisplayShots()">
-      <i class="icon-grid baku-button" />
-      <h4 class="baku-button">Plans</h4>
+      <i class="icon-grid baku-button"/>
+      <h4 class="baku-button">Plans - Plan {{ shotIndex }}</h4>
     </div>
-    <img class="shot-preview" alt="preview" ref="preview" />
+    <img class="shot-preview" alt="preview" ref="preview"/>
   </div>
 </template>
 
 <script lang="ts">
-import {
-  Component, Prop, Vue, Watch,
-} from 'vue-property-decorator';
-import { namespace } from 'vuex-class';
-import { Shot } from '@/utils/movie.service';
-import { Spinner } from '@/utils/spinner.class';
-import { ImageCacheService } from '@/utils/imageCache.service';
+  import { Component, Prop, Vue, Watch, } from 'vue-property-decorator';
+  import { namespace } from 'vuex-class';
+  import { Shot } from '@/utils/movie.service';
+  import { Spinner } from '@/utils/spinner.class';
+  import { ImageCacheService } from '@/utils/imageCache.service';
 
-const ProjectNS = namespace('project');
-const CaptureNS = namespace('capture');
-const WebRTCNS = namespace('webrtc');
+  const ProjectNS = namespace('project');
+  const CaptureNS = namespace('capture');
+  const WebRTCNS = namespace('webrtc');
 
-@Component
-export default class StoryboardPreviewComponent extends Vue {
-  @Prop()
-  public activeShot!: Shot;
+  @Component
+  export default class StoryboardPreviewComponent extends Vue {
+    @Prop()
+    public activeShot!: Shot;
 
-  @ProjectNS.State
-  public id!: string;
+    @Prop()
+    public shots!: Shot[];
 
-  @ProjectNS.Getter
-  public getActiveShotImgCount!: Shot;
+    @ProjectNS.State
+    public id!: string;
 
-  @CaptureNS.Action('resetState')
-  private resetCapture!: () => Promise<void>;
+    @ProjectNS.Getter
+    public getActiveShotImgCount!: Shot;
 
-  @WebRTCNS.Action('resetState')
-  private resetRTC!: () => Promise<void>;
+    @CaptureNS.Action('resetState')
+    private resetCapture!: () => Promise<void>;
 
-  public onDisplayShots() {
-    this.resetCapture();
-    this.resetRTC();
-    this.$router.push({
-      name: 'captureShots',
-      params: {
-        projectId: this.id,
-      },
-    });
-  }
+    @WebRTCNS.Action('resetState')
+    private resetRTC!: () => Promise<void>;
 
-  mounted() {
-    (this.$refs.preview as HTMLImageElement).src = this.getPreview();
-  }
+    get shotIndex() {
+      if (this.shots) {
+        return this.shots.findIndex(s => s.id === this.activeShot?.id) + 1;
+      } else {
+        return 0;
+      }
+    }
 
-  @Watch('getActiveShotImgCount')
-  public async onActiveShotImgCountChange(nb: number) {
-    if (nb) {
-      this.imageReady(this.activeShot.images[0].id);
+    public onDisplayShots() {
+      this.resetCapture();
+      this.resetRTC();
+      this.$router.push({
+        name: 'captureShots',
+        params: {
+          projectId: this.id,
+        },
+      });
+    }
+
+    mounted() {
+      (this.$refs.preview as HTMLImageElement).src = this.getPreview();
+    }
+
+    @Watch('getActiveShotImgCount')
+    public async onActiveShotImgCountChange(nb: number) {
+      if (nb) {
+        this.imageReady(this.activeShot.images[0].id);
+      }
+    }
+
+    public imageReady(imageId: string) {
+      if (this.activeShot && this.activeShot.images.length > 0) {
+        if (this.activeShot.images[0].id === imageId) {
+          (this.$refs.preview as HTMLImageElement).src = this.getPreview();
+        }
+      }
+    }
+
+    private getPreview() {
+      if (this.activeShot && this.activeShot.images.length > 0) {
+        const imageId = this.activeShot.images[0].id;
+        return ImageCacheService.getImage(imageId);
+      }
+      return Spinner;
     }
   }
+</script>
 
-  public imageReady(imageId: string) {
-    if (this.activeShot && this.activeShot.images.length > 0) {
-      if (this.activeShot.images[0].id === imageId) {
-        (this.$refs.preview as HTMLImageElement).src = this.getPreview();
+<style lang="scss">
+  .storyboard-preview-container {
+    max-width: 292px;
+
+    .storyboard-preview-header {
+      display: inline-flex;
+      align-items: center;
+      width: 100%;
+
+      h4 {
+        font-size: 28px;
+        font-weight: lighter;
+      }
+
+      i {
+        font-size: 28px;
+        padding-right: 10px;
       }
     }
   }
 
-  private getPreview() {
-    if (this.activeShot && this.activeShot.images.length > 0) {
-      const imageId = this.activeShot.images[0].id;
-      return ImageCacheService.getImage(imageId);
-    }
-    return Spinner;
+  .shot-preview {
+    width: 292px;
+    height: 164px;
   }
-}
-</script>
-
-<style lang="scss">
-.storyboard-preview-container {
-  max-width: 292px;
-
-  .storyboard-preview-header {
-    display: inline-flex;
-    align-items: center;
-    width: 100%;
-
-    h4 {
-      font-size: 28px;
-      font-weight: lighter;
-    }
-
-    i {
-      font-size: 28px;
-      padding-right: 10px;
-    }
-  }
-}
-
-.shot-preview {
-  width: 292px;
-  height: 164px;
-}
 </style>
