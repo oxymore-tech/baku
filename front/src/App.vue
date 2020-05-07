4dcf7ba8d226d7ad2ef41e942b717619830cba28
Jean-Baptiste Gamond <jeanbaptiste.gamond@livingobjects.com>
2020-05-07 10:52:38 +0200

    fix: shotname

--- front/src/App.vue
+++ front/src/App.vue
@@ -49,7 +49,7 @@
 
       <div class="flex-container" v-if="$route.name === 'captureShot' && activeShotIndex >= 0">
         <div class="previous-plan" @click="goToPreviousPlan()" title="Plan précédent">&lt;</div>
-        <div class="baku-button" @click="onOpenPlan()">Plan {{ activeShotIndex + 1 }}</div>
+        <div class="baku-button" @click="onOpenPlan()">{{movie.title}} - Plan {{ activeShotIndex + 1 }}</div>
         <div class="next-plan" @click="goToNextPlan()" title="Plan suivant">&gt;</div>
       </div>
       <div class="right-nav flex-container" :class="{'right-nav-home': $route.name === 'home'}">
