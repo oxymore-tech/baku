f80ec5d339ecbe79f3473b57664a629a9876e12e
Jean-Baptiste Gamond <jb.gamond@gmail.com>
2020-05-08 11:42:46 +0200

    fix: build

--- front/src/components/capture/CarrouselComponent.vue
+++ front/src/components/capture/CarrouselComponent.vue
@@ -386,10 +386,4 @@ export default class CarrouselComponent extends Vue {
     }
   }
 }
-
-async function asyncForEach(array: any[], callback: any) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
 </script>
