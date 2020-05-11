2942b2e25fb43e5fcbc6769b36f633885443cf63
Jerome HEBERT <jerome.hebert@livingobjects.com>
2020-05-11 13:58:44 +0200

    workflow menu TOTO - nom "capture" dynamique

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="app" :class="{'app-home': $route.name === 'home'}">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"/>
     <link
       rel="stylesheet"
       href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css"
@@ -19,117 +19,178 @@
 
           <b-dropdown-item class aria-role="listitem">
             <div class="option-logo" @click="onPageAccueil()">
-              <i class="icon-movie-start-01 baku-button" />
+              <i class="icon-movie-start-01 baku-button"/>
               <span>Aller à la page d'accueil</span>
             </div>
           </b-dropdown-item>
           <b-dropdown-item aria-role="listitem">
             <div class="option-logo" @click="onPersoFilm()">
-              <i class="icon-cog baku-button" />
+              <i class="icon-cog baku-button"/>
               <span>Personnaliser votre film</span>
             </div>
           </b-dropdown-item>
           <b-dropdown-item aria-role="listitem">
             <div class="option-logo" @click="onOpenLibrary()">
-              <i class="icon-cloud baku-button" />
+              <i class="icon-cloud baku-button"/>
               <span>Ouvrir ma librairie</span>
             </div>
           </b-dropdown-item>
           <b-dropdown-item aria-role="listitem">
             <div class="option-logo" @click="onOpenPlan()">
-              <i class="icon-grid baku-button" />
+              <i class="icon-grid baku-button"/>
               <span>Accéder aux plans</span>
             </div>
           </b-dropdown-item>
           <b-dropdown-item v-if="this.id" aria-role="listitem">
             <div class="option-logo" @click="onCreatePlan()">
-              <i class="icon-plus baku-button" />
+              <i class="icon-plus baku-button"/>
               <span>Créer un nouveau plan</span>
+            </div>
+          </b-dropdown-item>
+        </b-dropdown>
+
+
+        <b-dropdown append-to-body aria-role="list">
+          <div class="label-menu"  slot="trigger" role="button">
+            <p v-if="this.id"> {{ this.movie.title }}<i class="icon-angle-down baku-button" /></p>
+            <p v-else> Ma librairie<i class="icon-angle-down baku-button" /></p>
+          </div>
+
+          <b-dropdown-item class aria-role="listitem">
+            <div class="option-logo" @click="onClickMyLibrary()">
+              <i class="icon-folder-open-regular baku-button"/>
+              <span>Mes films</span>
+            </div>
+          </b-dropdown-item>
+
+          <b-dropdown-item class aria-role="listitem">
+            <div class="option-logo" @click="onClickMyLibrary()">
+              <i class="icon-movie baku-button"/>
+              <span>Mes films récents</span>
+            </div>
+          </b-dropdown-item>
+
+          <b-dropdown-item class aria-role="listitem">
+            <div class="option-logo" @click="onCreateProject()">
+              <i class="icon-plus baku-button"/>
+              <span>Nouveau film</span>
+            </div>
+          </b-dropdown-item>
+        </b-dropdown>
+
+        <b-dropdown append-to-body aria-role="list" v-if="this.id">
+          <div class="label-menu" slot="trigger" role="button">
+            <p>Capture <i class=" icon-angle-down baku-button"/></p>
+          </div>
+
+<!--          <b-dropdown-item class aria-role="listitem" v-if="this.activeShotId">-->
+          <b-dropdown-item class aria-role="listitem" :disabled="!this.activeShotId || $route.name === 'captureShot'">
+            <div class="option-logo" @click="activateShot()">
+              <i class="icon-camera baku-button"/>
+              <span>Capture</span>
+            </div>
+          </b-dropdown-item>
+
+          <b-dropdown-item class aria-role="listitem">
+            <div class="option-logo" @click="onOpenPlan()">
+              <i class="icon-grid baku-button"/>
+              <span>Plans</span>
+            </div>
+          </b-dropdown-item>
+
+          <b-dropdown-item class aria-role="listitem">
+            <div class="option-logo" @click="onCreatePlan()">
+              <i class="icon-plus baku-button"/>
+              <span>Nouveau plan</span>
             </div>
           </b-dropdown-item>
         </b-dropdown>
       </div>
 
-      <div
-        class="flex-container"
-        v-if="$route.name === 'captureShot' && activeShotIndex >= 0 || $route.name === 'movie'"
-      >
         <div
-          v-if="nbShot > 1 && $route.name === 'captureShot'"
-          class="previous-plan"
-          @click="goToPreviousPlan()"
-          title="Plan précédent"
-        >&lt;</div>
-        <div class="baku-button" @click="onOpenPlan()">
-          {{movie.title}}
-          <template
-            v-if="$route.name === 'captureShot' && activeShotIndex >= 0"
-          >- Plan {{ activeShotIndex + 1 }}</template>
+          class="flex-container"
+          v-if="$route.name === 'captureShot' && activeShotIndex >= 0 || $route.name === 'movie'"
+        >
+          <div
+            v-if="nbShot > 1 && $route.name === 'captureShot'"
+            class="previous-plan"
+            @click="goToPreviousPlan()"
+            title="Plan précédent"
+          >&lt;
+          </div>
+          <div class="baku-button" @click="onOpenPlan()">
+            {{movie.title}}
+            <template
+              v-if="$route.name === 'captureShot' && activeShotIndex >= 0"
+            >- Plan {{ activeShotIndex + 1 }}
+            </template>
+          </div>
+          <div
+            v-if="nbShot > 1 && $route.name === 'captureShot'"
+            class="next-plan"
+            @click="goToNextPlan()"
+            title="Plan suivant"
+          >&gt;
+          </div>
         </div>
-        <div
-          v-if="nbShot > 1 && $route.name === 'captureShot'"
-          class="next-plan"
-          @click="goToNextPlan()"
-          title="Plan suivant"
-        >&gt;</div>
-      </div>
 
-      <div class="flex-container">
-        <div class="flex-container bug" :class="{'bug-home': $route.name === 'home'}">
-          <div>
-            <i
-              class="icon-bullhorn-solid baku-button"
-              @click="openIssue()"
-              title="Reporter un bug"
-            />
+        <div class="flex-container">
+          <div class="flex-container bug" :class="{'bug-home': $route.name === 'home'}">
+            <div>
+              <i
+                class="icon-bullhorn-solid baku-button"
+                @click="openIssue()"
+                title="Reporter un bug"
+              />
+            </div>
+          </div>
+          <div class="flex-container right-nav" :class="{'right-nav-home': $route.name === 'home'}">
+            <div>
+              <i class="icon-user-dragon"/>
+            </div>
+            <div class="pseudo">{{ username }}</div>
           </div>
         </div>
-        <div class="flex-container right-nav" :class="{'right-nav-home': $route.name === 'home'}">
-          <div>
-            <i class="icon-user-dragon" />
-          </div>
-          <div class="pseudo">{{ username }}</div>
-        </div>
-      </div>
     </nav>
-    <router-view :key="$route.fullPath" />
+    <router-view :key="$route.fullPath"/>
   </div>
 </template>
 
-<style lang="scss" >
-@import "styles/style.scss";
-@import "styles/buefy.scss";
-@import "assets/style.css";
+<style lang="scss">
+  @import "styles/style.scss";
+  @import "styles/buefy.scss";
+  @import "assets/style.css";
 
-.left-nav {
-  display: flex;
-  align-items: center;
-}
+  .left-nav {
+    display: flex;
+    align-items: center;
+  }
 
-.movie-title {
-  font-size: 24px;
-  margin: 0 24px;
-  overflow: hidden;
-  /* height: 100%; */
-  text-overflow: clip;
-  white-space: nowrap;
-}
+  .movie-title {
+    font-size: 24px;
+    margin: 0 24px;
+    overflow: hidden;
+    /* height: 100%; */
+    text-overflow: clip;
+    white-space: nowrap;
+  }
 </style>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import { namespace } from "vuex-class";
-import ProjectSettingsPopup from "@/components/ProjectSettingsPopup.vue";
-import { Movie } from "@/utils/movie.service";
-import IssuePopup from "@/components/IssuePopup.vue";
+import { Component, Vue } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+import ProjectSettingsPopup from '@/components/ProjectSettingsPopup.vue';
+import { Movie } from '@/utils/movie.service';
+import IssuePopup from '@/components/IssuePopup.vue';
+import { createProject } from '@/api';
 
-const ProjectNS = namespace("project");
-const UserNS = namespace("user");
+const ProjectNS = namespace('project');
+const UserNS = namespace('user');
 
 @Component({
   components: {
-    ProjectSettingsPopup
-  }
+    ProjectSettingsPopup,
+  },
 })
 export default class App extends Vue {
   @ProjectNS.State
@@ -141,38 +202,35 @@ export default class App extends Vue {
   @UserNS.State
   public username!: string;
 
-  @UserNS.Getter("getPersonalisedProjectTitle")
+  @UserNS.Getter('getPersonalisedProjectTitle')
   public getPersonalisedProjectTitle!: string;
 
-  @ProjectNS.Action("createShot")
+  @ProjectNS.Action('createShot')
   private createShotAction!: (name?: string) => Promise<string>;
 
-  @ProjectNS.Action("changeFps")
+  @ProjectNS.Action('changeFps')
   protected changeFps!: (fps: number) => Promise<void>;
 
-  @ProjectNS.Action("updateTitle")
+  @ProjectNS.Action('updateTitle')
   protected updateTitle!: (title: string) => Promise<void>;
 
-  @ProjectNS.Getter("getPreviousShotId")
+  @ProjectNS.Getter('getPreviousShotId')
   protected previousShotId!: string;
 
-  @ProjectNS.Getter("getNextShotId")
+  @ProjectNS.Getter('getNextShotId')
   protected nextShotId!: string;
 
-  @ProjectNS.Getter("getActiveShotIndex")
+  @ProjectNS.Getter('getActiveShotIndex')
   public activeShotIndex!: Number;
 
-  @ProjectNS.Getter("getShotCount")
+  @ProjectNS.Getter('getShotCount')
   public nbShot!: Number;
 
-  public openProjectSettings() {
-    this.$buefy.modal.open({
-      parent: this,
-      component: ProjectSettingsPopup,
-      hasModalCard: true,
-      canCancel: ["escape", "outside"]
-    });
-  }
+  @ProjectNS.State
+  public activeShotId!: string;
+
+  @ProjectNS.Action('loadProject')
+  protected loadProjectAction!: (projectId: string) => Promise<void>;
 
   public pageName!: string;
 
@@ -182,30 +240,30 @@ export default class App extends Vue {
 
   public async onPageAccueil() {
     await this.$router.push({
-      name: "home"
+      name: 'home',
     });
   }
 
   public async onPersoFilm() {
     await this.$router.push({
-      name: "movieEditing"
+      name: 'movieEditing',
     });
   }
 
   public async onOpenLibrary() {
     await this.$router.push({
-      name: "library"
+      name: 'library',
     });
   }
 
   public async onOpenPlan() {
     await this.$router.push({
-      name: "movie"
+      name: 'movie',
     });
   }
 
   public async onCreatePlan() {
-    const shotId = await this.createShotAction("Nouveau plan");
+    const shotId = await this.createShotAction('Nouveau plan');
     await this.moveToShot(shotId);
   }
 
@@ -220,12 +278,12 @@ export default class App extends Vue {
   }
 
   private async moveToShot(shotId: string) {
-    return await this.$router.push({
-      name: "captureShot",
+    return this.$router.push({
+      name: 'captureShot',
       params: {
         projectId: this.id,
-        shotId
-      }
+        shotId,
+      },
     });
   }
 
@@ -234,8 +292,35 @@ export default class App extends Vue {
       parent: this,
       component: IssuePopup,
       hasModalCard: true,
-      canCancel: ["escape", "outside"]
+      canCancel: ['escape', 'outside'],
     });
+  }
+
+  public async onCreateProject() {
+    const projectId = await createProject();
+    await this.loadProjectAction(projectId);
+    const shotId = await this.createShotAction('Nouveau plan');
+    await this.changeFps(12);
+    await this.updateTitle(this.getPersonalisedProjectTitle);
+    this.$store.dispatch('project/changeActiveShot', shotId);
+    await this.$router.push({
+      name: 'captureShot',
+      params: {
+        projectId,
+        shotId,
+      },
+    });
+  }
+
+
+  public async onClickMyLibrary() {
+    await this.$router.push({
+      name: 'library',
+    });
+  }
+
+  public async activateShot() {
+    await this.moveToShot(this.activeShotId);
   }
 }
 </script>
