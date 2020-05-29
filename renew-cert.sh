8c47543e91c3c96c4509b97179da44e7a2620ad4
Augustin Gjini <augustin.gjini@pm.me>
2020-05-29 18:17:27 +0200

    renew cert script : WIP

---
+++ renew-cert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker-compose down
+
+docker run -ti -v letsencrypt:/etc/letsencrypt certbot/certbot certonly --standalone --email support@bakuanimation.com -d bakuanimation.com,studio.bakuanimation.com
+
+echo Enter password ?
+
+password=$(read)
+
+sudo openssl pkcs12 -export -out letsencrypt/server.pkcs12 -in letsencrypt/letsencrypt/live/app.bakuanimation.com/fullchain.pem -inkey letsencrypt/letsencrypt/live/app.bakuanimation.com/privkey.pem -passout pass:${password}
+
+docker-compose up -d
