5c5e25de57ca53fb14fcb5e8db3ae88ba4470ff0
Jean-Baptiste Gamond <jb.gamond@gmail.com>
2020-05-31 18:01:03 +0200

    fix: build pb

--- front/src/views/CaptureView.vue
+++ front/src/views/CaptureView.vue
@@ -378,7 +378,7 @@ export default class CaptureView extends AbstractProjectView {
     }
   }
 
-  private stopMoving() {
+  public stopMoving() {
     this.isPlaying = null;
     this.syncActiveFrame();
   }
