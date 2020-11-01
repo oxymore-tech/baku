efbd9b76fceb3644d4ea35888a322a2313580f67
Pascal Pratmarty <ppratmarty@liveaction.com>
2020-11-01 22:53:02 +0100

    [fix] App.vue: removed "&lt" sign when on first plan (no other plan before!)

--- front/src/App.vue
+++ front/src/App.vue
@@ -94,7 +94,7 @@
         v-if="$route.name === 'captureShot' && activeShotIndex >= 0 || $route.name === 'movie'"
       >
         <div
-          v-if="nbShot > 1 && $route.name === 'captureShot'"
+          v-if="nbShot > 1 && activeShotIndex > 0 && $route.name === 'captureShot'"
           class="previous-plan"
           @click="goToPreviousPlan()"
           title="Plan précédent"
