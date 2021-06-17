049e615a8dc3282396089742c043f42f7c68c37d
Quentin Meillier <quentin.meillier@telecomnancy.eu>
2021-06-17 09:13:22 +0200

    SW installable

--- front/public/service-worker.js
+++ front/public/service-worker.js
@@ -1,1 +1,8 @@
 console.log('Hello from service-worker.js');
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
