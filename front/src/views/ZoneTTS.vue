f129524c63344a9401e33b434a11b0901bff938f
Nathan R <nathan.razafimanantsoa@etu.enseeiht.fr>
2022-02-09 19:01:27 +0100

    Changement liste voix

--- front/src/views/ZoneTTS.vue
+++ front/src/views/ZoneTTS.vue
@@ -33,15 +33,9 @@ export default {
       }
       let voices = window.speechSynthesis.getVoices();
       voices.forEach(voice => {
-        const option = document.createElement('option'); // pour chaque langue on crée un élément
-        option.textContent = voice.name + '(' + voice.lang + ')'; // chaque voix = nom + langue
-
-        /* Attributs */
-        option.setAttribute('langue', voice.lang);
-        option.setAttribute('nom', voice.name);
-
-        this.voicesList.push(option);
+        this.voicesList.push(voice);
       })
+      this.voiceSelected = this.voicesList[0];
     }
   },
 
@@ -62,29 +56,31 @@ export default {
   <div class="TTS">
     <div class="wrapper main-component">
 
-  <!-- Zone de saisie du texte -->
+      <!-- Zone de saisie du texte -->
       <textarea class="textTTS centered" placeholder="Insérez votre texte ici..." maxlength="32760" v-model="currentMsg"></textarea>
 
 
-  <!-- Pitch et vitesse -->
+      <!-- Pitch et vitesse -->
       <div class="text-center wrapper">
         <p class="param">Paramètres audio</p>
         <div class='centered'>
-            <input type="range" min="0" max="2" step="0.1" v-model="pitch">
-            <label class="ranges">Ton : {{pitch}}</label>
+          <input type="range" min="0" max="2" step="0.1" v-model="pitch">
+          <label class="ranges">Ton : {{pitch}}</label>
         </div>
         <div class='centered'>
-            <input type="range" min="0.1" max="10" step="0.1" v-model="rate">
-            <label class="ranges">Vitesse : {{rate}} </label>
+          <input type="range" min="0.1" max="10" step="0.1" v-model="rate">
+          <label class="ranges">Vitesse : {{rate}} </label>
         </div>
       </div>
 
-  <!-- Saisie de la voix -->
+      <!-- Saisie de la voix -->
       <select class="voice-selector" v-model="voiceSelected">
-          <option v-for="v in voicesList" :value="v.textContent"> {{v.textContent}} </option>
+        <option v-for="v in voicesList" :value="v"> {{v.name + '(' + v.lang + ')'}} </option>
       </select>
+      <span>Selected : {{voiceSelected.name}}</span>
 
-  <!-- Boutons pour valider -->
+
+      <!-- Boutons pour valider -->
       <div class="wrapper centered">
         <button class="buttonTTS" v-on:click="listenAudio"> Ecouter l'audio </button>
         <button class="buttonTTS" v-on:click="generateAudio"> Enregistrer l'audio </button>
@@ -97,174 +93,174 @@ export default {
 
 <style scoped>
 
-  .TTS {
-    justify-content: center;
-    display: flex
-  }
+.TTS {
+  justify-content: center;
+  display: flex
+}
 
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 10px;
-  }
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+}
 
-  .main-component {
-    width: 600px;
-    border: #ffbd72;
-    border-style: solid;
-    padding: 10px;
-    background: #ffebd3;
-    border-radius: 16px;
-  }
+.main-component {
+  width: 600px;
+  border: #ffbd72;
+  border-style: solid;
+  padding: 10px;
+  background: #ffebd3;
+  border-radius: 16px;
+}
 
-  .param {
-    font-size: 16px;
-  }
+.param {
+  font-size: 16px;
+}
 
-  .ranges {
-    font-size: 13px;
-  }
+.ranges {
+  font-size: 13px;
+}
 
-  .textTTS {
-    padding: 12px 20px;
-    box-sizing: border-box;
-    border: 2px solid #ccc;
-    border-radius: 16px;
-    border-color: black;
-    background-color: #ffffff;
-    font-size: 16px;
-    width: 80%;
-    height: 175px;
-  }
+.textTTS {
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 16px;
+  border-color: black;
+  background-color: #ffffff;
+  font-size: 16px;
+  width: 80%;
+  height: 175px;
+}
 
-  .buttonTTS {
-    background-color: #d9cece;
-    padding: 2px 10px;
-    text-align: center;
-    color: #455054;
-    border: 2px solid white;
-    border-radius: 16px;
-    font-size: 1.8rem;
-    font-family: "Montserrat", Helvetica, Arial, sans-serif;
-    cursor: pointer;
-    margin: 10px;
-    border-style: solid;
-    border: #bfb4ac
-  }
+.buttonTTS {
+  background-color: #d9cece;
+  padding: 2px 10px;
+  text-align: center;
+  color: #455054;
+  border: 2px solid white;
+  border-radius: 16px;
+  font-size: 1.8rem;
+  font-family: "Montserrat", Helvetica, Arial, sans-serif;
+  cursor: pointer;
+  margin: 10px;
+  border-style: solid;
+  border: #bfb4ac
+}
 
-  .buttonTTS:hover {
-    background-color: #dcdcdc;
-    color: black;
-  }
+.buttonTTS:hover {
+  background-color: #dcdcdc;
+  color: black;
+}
 
-  label {
-      font: 1rem 'Fira Sans', sans-serif;
-      width: 75px;
-  }
+label {
+  font: 1rem 'Fira Sans', sans-serif;
+  width: 75px;
+}
 
-  .text-center {
-      text-align: center;
-  }
+.text-center {
+  text-align: center;
+}
 
-  .centered {
-      display: flex;
-      flex-direction: row-reverse;
-      margin: 5px;
-      justify-content:center
-  }
+.centered {
+  display: flex;
+  flex-direction: row-reverse;
+  margin: 5px;
+  justify-content:center
+}
 
-  .voice-selector {
-    width: 50%;
-    height: 30px;
-    border-radius: 16px;
-  }
+.voice-selector {
+  width: 50%;
+  height: 30px;
+  border-radius: 16px;
+}
 
-  input[type=range] {
-    -webkit-appearance: none;
-    margin: 18px 0;
-    width: 100%;
-  }
-  input[type=range]:focus {
-    outline: none;
-  }
-  input[type=range]::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 8.4px;
-    cursor: pointer;
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-    background: #7f7f7f;
-    border-radius: 1.3px;
-    border: 0.2px solid #010101;
-  }
-  input[type=range]::-webkit-slider-thumb {
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-    border: 1px solid #000000;
-    height: 36px;
-    width: 16px;
-    border-radius: 3px;
-    background: #ffbd72;
-    cursor: pointer;
-    -webkit-appearance: none;
-    margin-top: -14px;
-  }
-  input[type=range]:focus::-webkit-slider-runnable-track {
-    background: #7f7f7f;
-  }
-  input[type=range]::-moz-range-track {
-    width: 100%;
-    height: 8.4px;
-    cursor: pointer;
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-    background: #7f7f7f;
-    border-radius: 1.3px;
-    border: 0.2px solid #010101;
-  }
-  input[type=range]::-moz-range-thumb {
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-    border: 1px solid #000000;
-    height: 36px;
-    width: 16px;
-    border-radius: 3px;
-    background: #ffbd72;
-    cursor: pointer;
-  }
-  input[type=range]::-ms-track {
-    width: 100%;
-    height: 8.4px;
-    cursor: pointer;
-    background: transparent;
-    border-color: transparent;
-    border-width: 16px 0;
-    color: transparent;
-  }
-  input[type=range]::-ms-fill-lower {
-    background: #2a6495;
-    border: 0.2px solid #010101;
-    border-radius: 2.6px;
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-  }
-  input[type=range]::-ms-fill-upper {
-    background: #7f7f7f;
-    border: 0.2px solid #010101;
-    border-radius: 2.6px;
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-  }
-  input[type=range]::-ms-thumb {
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-    border: 1px solid #000000;
-    height: 36px;
-    width: 16px;
-    border-radius: 3px;
-    background: #ffbd72;
-    cursor: pointer;
-  }
-  input[type=range]:focus::-ms-fill-lower {
-    background: #7f7f7f;
-  }
-  input[type=range]:focus::-ms-fill-upper {
-    background: #7f7f7f;
-  }
+input[type=range] {
+  -webkit-appearance: none;
+  margin: 18px 0;
+  width: 100%;
+}
+input[type=range]:focus {
+  outline: none;
+}
+input[type=range]::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 8.4px;
+  cursor: pointer;
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  background: #7f7f7f;
+  border-radius: 1.3px;
+  border: 0.2px solid #010101;
+}
+input[type=range]::-webkit-slider-thumb {
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  border: 1px solid #000000;
+  height: 36px;
+  width: 16px;
+  border-radius: 3px;
+  background: #ffbd72;
+  cursor: pointer;
+  -webkit-appearance: none;
+  margin-top: -14px;
+}
+input[type=range]:focus::-webkit-slider-runnable-track {
+  background: #7f7f7f;
+}
+input[type=range]::-moz-range-track {
+  width: 100%;
+  height: 8.4px;
+  cursor: pointer;
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  background: #7f7f7f;
+  border-radius: 1.3px;
+  border: 0.2px solid #010101;
+}
+input[type=range]::-moz-range-thumb {
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  border: 1px solid #000000;
+  height: 36px;
+  width: 16px;
+  border-radius: 3px;
+  background: #ffbd72;
+  cursor: pointer;
+}
+input[type=range]::-ms-track {
+  width: 100%;
+  height: 8.4px;
+  cursor: pointer;
+  background: transparent;
+  border-color: transparent;
+  border-width: 16px 0;
+  color: transparent;
+}
+input[type=range]::-ms-fill-lower {
+  background: #2a6495;
+  border: 0.2px solid #010101;
+  border-radius: 2.6px;
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+}
+input[type=range]::-ms-fill-upper {
+  background: #7f7f7f;
+  border: 0.2px solid #010101;
+  border-radius: 2.6px;
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+}
+input[type=range]::-ms-thumb {
+  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  border: 1px solid #000000;
+  height: 36px;
+  width: 16px;
+  border-radius: 3px;
+  background: #ffbd72;
+  cursor: pointer;
+}
+input[type=range]:focus::-ms-fill-lower {
+  background: #7f7f7f;
+}
+input[type=range]:focus::-ms-fill-upper {
+  background: #7f7f7f;
+}
 
 
 </style>
