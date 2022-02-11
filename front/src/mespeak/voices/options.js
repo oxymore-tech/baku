71ef32c81cd3e106990d76fd1a5da3dcbe44afbc
Nathan R <nathan.razafimanantsoa@etu.enseeiht.fr>
2022-02-11 15:22:43 +0100

    Ajout de voix différentes

---
+++ front/src/mespeak/voices/options.js
@@ -0,0 +1,47 @@
+export default {
+  femme1 : {
+    "id" : "femme1",
+    "voix": "f1"
+  },
+  femme2 : {
+    "id" : "femme2",
+    "voix": "f2"
+  },
+  femme3 : {
+    "id" : "femme3",
+    "voix": "f3"
+  },
+  femme4 : {
+    "id" : "femme4",
+    "voix": "f4"
+  },
+  homme1 : {
+    "id" : "homme1",
+    "voix": "m1"
+  },
+  homme2 : {
+    "id" : "homme2",
+    "voix": "m2"
+  },
+  homme3 : {
+    "id" : "homme3",
+    "voix": "m3"
+  },
+  homme4 : {
+    "id" : "homme4",
+    "voix": "m4"
+  },
+  homme5 : {
+    "id" : "homme5",
+    "voix": "m5"
+  },
+  homme6 : {
+    "id" : "homme6",
+    "voix": "m6"
+  },
+  homme7 : {
+    "id" : "homme7",
+    "voix": "m7"
+  },
+
+}
