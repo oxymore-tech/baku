8e719b382092d3dfd03e933cf8040d006c50feca
lquint <leo.quintrand@gmail.com>
2022-02-16 15:54:53 +0100

    premier test

---
+++ back/src/main/java/com/bakuanimation/rest/TTSController.java
@@ -0,0 +1,64 @@
+package com.bakuanimation.rest;
+
+import com.bakuanimation.api.ImageService;
+import com.bakuanimation.api.PermissionService;
+import com.bakuanimation.api.SoundService;
+import com.bakuanimation.model.Movie;
+import com.bakuanimation.model.Project;
+import com.bakuanimation.service.HistoryServiceImpl;
+import com.bakuanimation.service.PathService;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.multipart.StreamingFileUpload;
+import io.micronaut.http.server.types.files.StreamedFile;
+import io.micronaut.http.server.types.files.SystemFile;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.io.*;
+import java.nio.file.Files;
+
+@Controller("/tts")
+public class SoundController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SoundController.class);
+
+    private final HistoryServiceImpl historyService;
+    private final SoundService soundService;
+    private final PathService pathService;
+    private final PermissionService permissionService;
+
+    public TTSController(HistoryServiceImpl historyService, ImageService imageService,
+                           PathService pathService, PermissionService permissionService, TTSService ttsService) {
+        this.historyService = historyService;
+        this.ttsService = ttsService;
+        this.pathService = pathService;
+        this.permissionService = permissionService;
+    }
+
+    @Post(value = "/tts/test/save", consumes = MediaType.MULTIPART_FORM_DATA)
+    public HttpResponse<?> saveWav(){
+        this.ttsService.generateVoice("Ceci est un test","test","upmc-pierre-hsmm");
+        return HttpResponse.status(HttpStatus.CREATED).body(new Message(HttpStatus.getCode(),"Saved Successfully !"));
+    }
+       
+    }
+
+
+    @Get("/tts/test/save")
+    public HttpResponse<Object> getWav()
+        return HttpResponse.ok(new SystemFile("/api/tts/test/save".toFile()).attach(test));
+        
+    }
+
+
+}
